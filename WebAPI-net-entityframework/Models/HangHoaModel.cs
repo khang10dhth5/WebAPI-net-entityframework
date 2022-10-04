fcf86b8b4249f261de742ffaf1e23b81c30756ca
Nguyen Khang <khang10dhth5@gmail.com>
2022-10-04 19:16:01 +0700

    themhanghoa

---
+++ WebAPI-net-entityframework/Models/HangHoaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_net_entityframework.Models
+{
+    public class HangHoaModel
+    {
+        public string TenHH { get; set; }
+        public string MoTa { get; set; }
+        public double DonGia { get; set; }
+    }
+}
